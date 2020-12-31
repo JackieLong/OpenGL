d1a6b633189bff8e9faf29546fee8b04f4f7739e
Jackie <741212080@qq.com>
2020-12-31 16:15:31 +0800

    Revert "Revert "添加纹理使用示例代码""
    This reverts commit 310ca0a9702b1b24ce3bd24881c9d20dfab75842.

--- project_VBO_EBO_VAO/src/test.cpp
+++ project_VBO_EBO_VAO/src/test.cpp
@@ -1,8 +1,6 @@
-﻿#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <iostream>
+﻿#include <iostream>
 #include <functional>
+#include "project_lib.h"
 
 void framebuffer_size_callback( GLFWwindow *window, int width, int height );
 void initGLFW();
---
+++ project_lib/src/project_lib.h
@@ -0,0 +1,5 @@
+﻿#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "utils.h"
+#include "shader.h"
