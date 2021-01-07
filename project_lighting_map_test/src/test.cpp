e3d761d728968997208b40b67d6fa8dcc2ece764
Jackie <741212080@qq.com>
2021-01-07 17:48:23 +0800

    材质学习例子

---
+++ project_lighting_map_test/src/test.cpp
@@ -0,0 +1,361 @@
+﻿#include "project_lib.h"
+#include "other.h"
+
+using namespace std;
+
+Camera *pCamera      = nullptr;                         // 摄像机
+glm::vec3 lightPos;                                     // 光源位置
+glm::vec3 lightColor = glm::vec3( 1.0f, 1.0f, 1.0f );   // 光源颜色
+
+void loadMatrixTarget( ShaderProgram &shaderProgram );
+void loadMatrixLight( ShaderProgram &shaderProgram );
+void loadVertexDataObject( GLuint *VAO, GLuint *VBO );
+GLint loadTextureDiffuse( const ShaderProgram &shaderProgram );
+GLint loadTextureSpecular( const ShaderProgram &shaderProgram );
+GLint loadTextureGlow( const ShaderProgram &shaderProgram );
+
+int main()
+{
+    pCamera = new Camera( glm::vec3( 1.0f, 1.0f, 7.0f ),            // 摄像机的初始位置
+                          glm::vec3( 0.5f, 0.5f, 0.0f ),            // 摄像机注视点
+                          glm::vec3( 0.0f, 1.0f, 0.0f ),            // 世界坐标中的向上向量
+                          -90.f,                                    // Yaw
+                          0.0f );                                   // Pitch
+    GLFWwindow *window = init( pCamera );               // 鼠标滚轮回调
+
+    GLuint VAO = 0, VBO = 0;                            // VBO顶点缓冲对象，VAO顶点数组对象，EBO索引缓冲对象
+    GLuint VAO1 = 0, VBO1 = 0;                          // VBO顶点缓冲对象，VAO顶点数组对象，EBO索引缓冲对象
+    GLuint VAO2 = 0, VBO2 = 0;
+    loadVertexDataObject( &VAO, &VBO );                 // 加载物体顶点
+    loadVertexDataCoord( &VAO1, &VBO1 );                // 加载坐标轴顶点
+    loadVertexDataLight( &VAO2, &VBO2 );                // 加载模拟光源物体顶点
+
+    GLint sampler1 = loadTextureDiffuse( shaderProgram );           // 加载漫反射贴图
+    GLint sampler2 = loadTextureSpecular( shaderProgram );          // 加载镜面反射贴图
+    GLint sampler3 = loadTextureGlow( shaderProgram );              // 放射光贴图
+
+
+    renderLoop( window, [&]                                         // 渲染循环
+    {
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );                     // 状态设置函数：设置清空屏幕后，颜色缓冲区填充的颜色
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );       // 状态使用函数：清空颜色缓冲区和深度缓冲区
+
+        shaderProgramLight.use();
+        shaderProgramLight.setVec3( "lightColor", lightColor );
+        loadMatrixLight( shaderProgramLight );
+        glBindVertexArray( VAO2 );
+        glDrawArrays( GL_TRIANGLES, 0, 36 );
+
+        shaderProgramCoord.use();
+        loadMatrixTarget( shaderProgramCoord );
+        glBindVertexArray( VAO1 );
+        glDrawArrays( GL_LINES, 0, 6 );
+
+        shaderProgram.use();
+
+        shaderProgram.setVec3( "viewPos", pCamera->pos() );             // 观察位置
+
+        shaderProgram.setInt( "material.diffuse", sampler1 );
+        shaderProgram.setInt( "material.specular", sampler2 );
+        //shaderProgram.setVec3( "material.specular", 0.9f, 0.9f, 0.9f );
+        shaderProgram.setInt( "material.glow", sampler3 );
+        shaderProgram.setFloat( "material.shininess", 64.0f );
+
+        glm::vec3 diffuseColor = lightColor   * glm::vec3( 0.5f );      // 降低影响
+        glm::vec3 ambientColor = diffuseColor * glm::vec3( 0.2f );      // 很低的影响
+
+        shaderProgram.setVec3( "light.pos", lightPos );
+        shaderProgram.setVec3( "light.ambient", ambientColor );
+        shaderProgram.setVec3( "light.diffuse", diffuseColor );         // 将光照调暗了一些以搭配场景
+
+        shaderProgram.setVec3( "light.specular", 1.0f, 1.0f, 1.0f );
+
+        loadMatrixTarget( shaderProgram );
+        glBindVertexArray( VAO );
+        glDrawArrays( GL_TRIANGLES, 0, 36 );
+    } );
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    glDeleteVertexArrays( 1, &VAO );
+    glDeleteVertexArrays( 1, &VAO1 );
+    glDeleteVertexArrays( 1, &VAO2 );
+    glDeleteBuffers( 1, &VBO );
+    glDeleteBuffers( 1, &VBO1 );
+    glDeleteBuffers( 1, &VBO2 );
+    glDeleteBuffers( 1, &EBO );
+    glDeleteBuffers( 1, &EBO1 );
+    glDeleteBuffers( 1, &EBO2 );
+    glDeleteProgram( shaderProgram.id );
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+
+void loadMatrixTarget( ShaderProgram &shaderProgram )
+{
+    // 模型变换矩阵，glm0.9.9版本之后，注意一定要显式初始化为单位矩阵，很容易错误默认初始化为零矩阵。
+    glm::mat4 modelMatrix( 1.0f );
+
+    // 视图变换矩阵
+    glm::mat4 viewMatrix = pCamera->getViewMatrix();
+
+    // 投影变换矩阵（透视投影）
+    glm::mat4 projectionMatrix( 1.0f );
+    projectionMatrix = glm::perspective( glm::radians( ( float ) pCamera->fov() ), // 就是FOV，field of view，视角广度
+                                         ( float )ScreenWidth / ScreenHeight,      // aspectRatio，宽高比
+                                         0.1f,                                     // near plane，近平面位置
+                                         100.0f );                                 // far plane，远平面位置
+
+    shaderProgram.use();
+    shaderProgram.setMat4( "modelMatrix", glm::value_ptr( modelMatrix ) );
+    shaderProgram.setMat4( "viewMatrix", glm::value_ptr( viewMatrix ) );
+    shaderProgram.setMat4( "projectionMatrix", glm::value_ptr( projectionMatrix ) );
+}
+
+void loadMatrixLight( ShaderProgram &shaderProgram )
+{
+    // 模型变换矩阵，glm0.9.9版本之后，注意一定要显式初始化为单位矩阵，很容易错误默认初始化为零矩阵。
+    glm::mat4 modelMatrix( 1.0f );      // 显示坐标轴，不需要进行平移和旋转。
+
+    lightPos = glm::vec3( 0.0f, 1.0f, 0.0f ) +
+               glm::vec3( 2 * sin( 1.5f * glfwGetTime() ),
+                          0,
+                          2 * cos( 1.5f * glfwGetTime() ) );
+    modelMatrix = glm::translate( modelMatrix, lightPos );
+    modelMatrix = glm::scale( modelMatrix, glm::vec3( 0.3f ) );
+
+    glm::mat4 viewMatrix = pCamera->getViewMatrix();
+
+    // 投影变换矩阵（透视投影）
+    glm::mat4 projectionMatrix( 1.0f );
+    projectionMatrix = glm::perspective( glm::radians( ( float ) pCamera->fov() ), // 就是FOV，field of view，视角广度
+                                         ( float ) ScreenWidth / ScreenHeight,     // aspectRatio，宽高比
+                                         0.1f,                                     // near plane，近平面位置
+                                         100.0f );                                 // far plane，远平面位置
+
+    shaderProgram.use();
+    shaderProgram.setMat4( "modelMatrix", glm::value_ptr( modelMatrix ) );
+    shaderProgram.setMat4( "viewMatrix", glm::value_ptr( viewMatrix ) );
+    shaderProgram.setMat4( "projectionMatrix", glm::value_ptr( projectionMatrix ) );
+}
+
+// 构建第一张图的纹理
+GLint loadTextureDiffuse( const ShaderProgram &shaderProgram )
+{
+    string projDir = projectDir();
+
+    GLuint texture;
+    glGenTextures( 1, &texture );
+
+    glActiveTexture( GL_TEXTURE0 + 0 );         // 纹理数据由GL_TEXTURE0纹理单元管理
+    glBindTexture( GL_TEXTURE_2D, texture );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );  // “X”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );  // “Y”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );     // 纹理缩小时，采用GL_LINEAR策略
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );     // 纹理放大时，采用GL_LINEAR策略
+
+    GLint width, height, nrChannels;
+
+    // 加载图片到内存中
+    unsigned char *data = stbi_load( ( projDir + "\\res\\container2_diffuse.png" ).c_str(),    // 图片路径
+                                     &width, &height,                                   // 图片宽高
+                                     &nrChannels, 0 );                                  // 颜色通道数量
+    if( data )
+    {
+        // 上传纹理数据到显存中，
+        glTexImage2D( GL_TEXTURE_2D,        // 纹理数据又当前绑定的纹理单元管理
+                      0,                    // mipmap level，0表示一级，如果有多级，需要挨个单独设置
+                      GL_RGBA,              // 告诉 OpenGL 我们希望把纹理储存为何种格式
+                      width, height,        // 纹理宽高
+                      0,                    // 总为0，历史遗留问题
+                      GL_RGBA,              // 图像像素的分量组成
+                      GL_UNSIGNED_BYTE,     // 每个像素分量的大小
+                      data );               // 内存中的图像数据
+
+        glGenerateMipmap( GL_TEXTURE_2D );  // 动态生成多级纹理，有性能损耗，可以由外部工具预生成。
+    }
+    else
+    {
+        cout << "Failed to load texture" << endl;
+    }
+    stbi_image_free( data );                // 纹理数据已经上传到显存中，内存中的数据可以删除了。
+    return  0;
+}
+
+// 构建第一张图的纹理
+GLint loadTextureSpecular( const ShaderProgram &shaderProgram )
+{
+    string projDir = projectDir();
+
+    GLuint texture;
+    glGenTextures( 1, &texture );
+
+    glActiveTexture( GL_TEXTURE0 + 1 );         // 纹理数据由GL_TEXTURE0纹理单元管理
+    glBindTexture( GL_TEXTURE_2D, texture );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );  // “X”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );  // “Y”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );     // 纹理缩小时，采用GL_LINEAR策略
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );     // 纹理放大时，采用GL_LINEAR策略
+
+    GLint width, height, nrChannels;
+
+    // 加载图片到内存中
+    //unsigned char *data = stbi_load( ( projDir + "\\res\\container2_specular_color.png" ).c_str(),    // 图片路径
+    unsigned char *data = stbi_load( ( projDir + "\\res\\container2_specular.png" ).c_str(),    // 图片路径
+                                     &width, &height,                                   // 图片宽高
+                                     &nrChannels, 0 );                                  // 颜色通道数量
+
+    if( data )
+    {
+        // 上传纹理数据到显存中，
+        glTexImage2D( GL_TEXTURE_2D,        // 纹理数据又当前绑定的纹理单元管理
+                      0,                    // mipmap level，0表示一级，如果有多级，需要挨个单独设置
+                      GL_RGBA,              // 告诉 OpenGL 我们希望把纹理储存为何种格式
+                      width, height,        // 纹理宽高
+                      0,                    // 总为0，历史遗留问题
+                      GL_RGBA,              // 图像像素的分量组成
+                      GL_UNSIGNED_BYTE,     // 每个像素分量的大小
+                      data );               // 内存中的图像数据
+
+        glGenerateMipmap( GL_TEXTURE_2D );  // 动态生成多级纹理，有性能损耗，可以由外部工具预生成。
+    }
+    else
+    {
+        cout << "Failed to load texture" << endl;
+    }
+    stbi_image_free( data );                // 纹理数据已经上传到显存中，内存中的数据可以删除了。
+    return 1;
+}
+
+// 构建第一张图的纹理
+GLint loadTextureGlow( const ShaderProgram &shaderProgram )
+{
+    string projDir = projectDir();
+
+    GLuint texture;
+    glGenTextures( 1, &texture );
+
+    glActiveTexture( GL_TEXTURE0 + 2 );         // 纹理数据由GL_TEXTURE0纹理单元管理
+    glBindTexture( GL_TEXTURE_2D, texture );
+
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );  // “X”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );  // “Y”轴方向上的Wrapping方式
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );     // 纹理缩小时，采用GL_LINEAR策略
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );     // 纹理放大时，采用GL_LINEAR策略
+
+    GLint width, height, nrChannels;
+
+    unsigned char *data = stbi_load( ( projDir + "\\res\\container2_glow.jpg" ).c_str(),    // 图片路径
+                                     &width, &height,                                       // 图片宽高
+                                     &nrChannels, 0 );                                      // 颜色通道数量
+
+    if( data )
+    {
+        // 上传纹理数据到显存中，
+        glTexImage2D( GL_TEXTURE_2D,        // 纹理数据又当前绑定的纹理单元管理
+                      0,                    // mipmap level，0表示一级，如果有多级，需要挨个单独设置
+                      GL_RGB,               // 告诉 OpenGL 我们希望把纹理储存为何种格式
+                      width, height,        // 纹理宽高
+                      0,                    // 总为0，历史遗留问题
+                      GL_RGB,               // 图像像素的分量组成
+                      GL_UNSIGNED_BYTE,     // 每个像素分量的大小
+                      data );               // 内存中的图像数据
+
+        glGenerateMipmap( GL_TEXTURE_2D );  // 动态生成多级纹理，有性能损耗，可以由外部工具预生成。
+    }
+    else
+    {
+        cout << "Failed to load texture" << endl;
+    }
+    stbi_image_free( data );                // 纹理数据已经上传到显存中，内存中的数据可以删除了。
+    return 2;
+}
+
+void loadVertexDataObject( GLuint *VAO, GLuint *VBO )
+{
+    // 6个顶点（6个面 x 每个面有2个三角形组成 x 每个三角形有3个顶点）
+    // 这里没有使用索引缓冲对象
+    float vertices[] =
+    {
+        // 顶点坐标             // 法向量            // 纹理坐标
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,   0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
+    };
+
+    glGenVertexArrays( 1, VAO );
+    glBindVertexArray( *VAO );
+
+    glGenBuffers( 1, VBO );
+    glBindBuffer( GL_ARRAY_BUFFER, *VBO );
+    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+    glVertexAttribPointer( 0,                       // 顶点属性（3D坐标）位置值，指定vertex attribute的位置值，这个值对应在顶点着色器中的location=0的属性
+                           3,                       // 顶点属性的分量数量（number of component）
+                           GL_FLOAT,                // 分量数据类型（data type of component）
+                           GL_FALSE,                // 数据是否需要被标准化
+                           8 * sizeof( float ),     // 步长Stride，属性间隔，注意是两个属性值相同位置的间隔，不是首尾间隔。
+                           ( void * ) 0 );          // 数据在缓冲中起始位置的偏移量
+
+    glVertexAttribPointer( 1,                       // 顶点属性（法向量）位置值
+                           3,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           8 * sizeof( float ),
+                           ( void * )( 3 * sizeof( float ) ) );
+
+    glVertexAttribPointer( 2,                       // 顶点属性（纹理坐标）位置值
+                           2,
+                           GL_FLOAT,
+                           GL_FALSE,
+                           8 * sizeof( float ),
+                           ( void * )( 6 * sizeof( float ) ) );
+
+    glEnableVertexAttribArray( 0 );     // 顶点属性配置默认是关闭的，要记得打开，0对应layout(location=0)顶点属性
+    glEnableVertexAttribArray( 1 );     //
+    glEnableVertexAttribArray( 2 );     //
+
+    glBindVertexArray( 0 );
+}
