9d8882e1e92cfae5f79930ee888daaa29e0000d5
Jackie <741212080@qq.com>
2021-01-15 16:58:45 +0800

    帧缓冲学习例子

---
+++ project_framebuffer_test/src/data.h
@@ -0,0 +1,215 @@
+﻿#ifndef __DATA_H__
+#define __DATA_H__
+
+#include "project_lib.h"
+
+using namespace std;
+
+const int ScreenWidth = 800;
+const int ScreenHeight = 600;
+
+const float cubeVertices[] =        // 箱子顶点数据
+{
+    // positions          // texture Coords
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+
+const float floorVertices[] =       // 地板顶点数据
+{
+    // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+    5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+    -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+    5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+    -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+    5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+};
+
+const float coordVertices[] =   // 坐标轴顶点数据
+{
+    // 顶点坐标             //纹理坐标      //颜色
+    -10000.0f, 0.0f, 0.0f,  0.0f, 0.0f,     1.0f, 0.0f, 0.0f,   // X轴上顶点位置1
+    10000.0f, 0.0f, 0.0f,   0.0f, 0.0f,     1.0f, 0.0f, 0.0f,   // X轴上顶点位置2
+
+    0.0f, 10000.0f, 0.0f,   0.0f, 0.0f,     0.0f, 1.0f, 0.0f,   // Y轴上顶点位置1
+    0.0f, -10000.0f, 0.0f,  0.0f, 0.0f,     0.0f, 1.0f, 0.0f,   // Y轴上顶点位置2
+
+    0.0f, 0.0f, 10000.0f,   0.0f, 0.0f,     0.0f, 0.0f, 1.0f,   // Z轴上顶点位置1
+    0.0f, 0.0f, -10000.0f,  0.0f, 0.0f,     0.0f, 0.0f, 1.0f,   // Z轴上顶点位置2
+};
+
+// NDC坐标，[-1.0, 1.0]之间
+const float screenVertices[] =
+{
+    // positions          // texCoords
+    -1.0f,  1.0f, 0.0f,   0.0f, 1.0f,
+    -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,    1.0f, 0.0f,
+
+    -1.0f,  1.0f, 0.0f,   0.0f, 1.0f,
+    1.0f, -1.0f, 0.0f,    1.0f, 0.0f,
+    1.0f,  1.0f, 0.0f,    1.0f, 1.0f
+};
+
+enum SHADER_TASK
+{
+    TASK_COORD = 0,
+    TASK_OBJECT,
+    TASK_SCREEN,
+    TASK_END
+};
+
+Camera *pCamera = nullptr;
+
+double deltaTime = 0.0;             // 当前帧与上一帧的时间差
+double lastFrame = 0.0;             // 上一帧的时间
+
+Shader shader;                      // 着色器
+
+GLuint VAO_cube = 0;                // 箱子的VAO、VBO
+GLuint VBO_cube = 0;
+
+GLuint VAO_coord = 0;               // 坐标系的VAO、VBO
+GLuint VBO_coord = 0;
+
+GLuint VAO_floor = 0;               // 地板的VAO、VBO
+GLuint VBO_floor = 0;
+
+GLuint VAO_screen = 0;               // 地板的VAO、VBO
+GLuint VBO_screen = 0;
+
+GLuint textureCube = -1;            // 箱子纹理
+GLuint textureFloor = -1;           // 地板纹理
+
+GLuint frameBuffer = -1;            // 帧缓冲
+GLuint textureColorBuffer;
+
+glm::mat4 modelMatrix( 1.0f );       // 观察矩阵
+glm::mat4 viewMatrix( 1.0f );       // 观察矩阵
+glm::mat4 projectionMatrix( 1.0f ); // 投影矩阵
+
+
+GLuint createFrameBuffer();
+
+void loadData()
+{
+    pCamera = new Camera( glm::vec3( 0.0f, 0.0f, 3.0f ),    // 摄像机的初始位置
+                          glm::vec3( 0.0f, 0.0f, 0.0f ),    // 摄像机注视点
+                          glm::vec3( 0.0f, 1.0f, 0.0f ),    // 世界坐标中的向上向量
+                          -90.f,                            // Yaw
+                          0.0f );                           // Pitch
+
+    shader = Shader( projectDir() + "\\src\\shader\\shader_vertex", projectDir() + "\\src\\shader\\shader_fragment" );
+
+    createVertexBuffer( cubeVertices, sizeof( cubeVertices ), "32", &VAO_cube, &VBO_cube );
+    createVertexBuffer( floorVertices, sizeof( floorVertices ), "32", &VAO_floor, &VBO_floor );
+    createVertexBuffer( coordVertices, sizeof( coordVertices ), "323", &VAO_coord, &VBO_coord );
+    createVertexBuffer( screenVertices, sizeof( screenVertices ), "32", &VAO_screen, &VBO_screen );
+
+    textureFloor = createTexture( projectDir() + "/res/metal.png" );
+    textureCube = createTexture( projectDir() + "/res/marble.jpg" );
+
+    frameBuffer = createFrameBuffer();
+}
+
+// 创建自定义帧缓冲例子
+GLuint createFrameBuffer()
+{
+    // ***************************************************
+    // ******* 第一步，创建帧缓冲
+    // ***************************************************
+    GLuint frameBuffer;
+    glGenFramebuffers( 1, &frameBuffer );               // 创建自己的帧缓冲
+    glBindFramebuffer( GL_FRAMEBUFFER, frameBuffer );   // 暂时绑定到当前帧缓冲（代替默认帧缓冲），目的是保证后续的附件绑定能生效，在最后别忘记恢复到默认的帧缓冲
+
+    // ***************************************************
+    // ******* 第二步，绑定颜色附件，创建的纹理类型，原因是后续我们需要进行采样（读取数据）。
+    // ***************************************************
+    glGenTextures( 1, &textureColorBuffer );
+    glBindTexture( GL_TEXTURE_2D, textureColorBuffer ); // 暂时绑定到当前激活纹理单元，目的是保证后面开辟纹理内存能生效，最后别忘了恢复成默认
+
+    // 开辟纹理内存
+    glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB,     // 和纹理一样的参数类型
+                  ScreenWidth, ScreenHeight,    // 设置为一个指定大小（比如屏幕大小）
+                  0,                            // 总是0
+                  GL_RGB, GL_UNSIGNED_BYTE,     // 和纹理一样的参数类型
+                  NULL );                       // 空纹理数据，暂不填充
+
+    // 和普通纹理一样，设置纹理参数
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+    glBindTexture( GL_TEXTURE_2D, 0 );          // 别忘了恢复成默认的，到这一步，纹理附件已经创建完成
+
+    // 绑定到我们创建的帧缓冲中
+    glFramebufferTexture2D( GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorBuffer, 0 );
+
+    // ***************************************************
+    // ******* 第三步，绑定一个深度和模板附件（深度和模板数据都保存在一块内存中）,使用的渲染缓冲类型，原因是我们只会写入数据，不进行采样读取，使用渲染缓冲，性能会非常好。
+    // ***************************************************
+    GLuint renderbuffer;
+    glGenRenderbuffers( 1, &renderbuffer );
+    glBindRenderbuffer( GL_RENDERBUFFER, renderbuffer );    // 暂时绑定，目的保证下面的开辟内存能生效
+
+    glRenderbufferStorage( GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, ScreenWidth, ScreenHeight );
+
+    glBindRenderbuffer( GL_RENDERBUFFER, 0 );               // 恢复成默认绑定
+
+    glFramebufferRenderbuffer( GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, renderbuffer );   // 绑定渲染缓冲
+
+    // ***************************************************
+    // ******* 第四步：检查帧缓冲的完整性，是否所有必需的工作都做完了，绑定附件就是必须的。
+    // ***************************************************
+    if( glCheckFramebufferStatus( GL_FRAMEBUFFER ) != GL_FRAMEBUFFER_COMPLETE )
+    {
+        cout << "ERROR::FRAMEBUFFER:: FRAMEBUFFER is not complete" << endl;
+    }
+
+    glBindFramebuffer( GL_FRAMEBUFFER, 0 );     // 恢复到默认的帧缓冲，后续有需要，随时绑定使用。
+
+    return frameBuffer;
+}
+
+#endif
---
+++ project_framebuffer_test/src/test.cpp
@@ -0,0 +1,153 @@
+﻿#include "project_lib.h"
+#include "data.h"
+
+using namespace std;
+
+void renderLoop( GLFWwindow *window, function<void()> renderCallback );
+void processInput( GLFWwindow *window );
+void drawToCustomFrameBuffer();
+void drawToDefaultFrameBuffer();
+
+int main()
+{
+    initGLFW();     // 初始化GLFW
+
+    GLFWwindow *window = createWindow( ScreenWidth,
+                                       ScreenHeight,
+                                       "LearnOpenGL",
+                                       []( GLFWwindow * window, double xpos, double ypos )      // 鼠标滑动回调
+    {
+        pCamera->processMouseMovement( window, xpos, ypos );
+    },
+    []( GLFWwindow * window, double xoffset, double yoffset )       // 鼠标滚轮回调
+    {
+        pCamera->processMouseScroll( window, xoffset, yoffset );
+    } );
+
+    initGLAD();     // 初始化GLAD
+
+    loadData();     // 加载顶点数据、纹理数据等
+
+    renderLoop( window, [&]                                     // 渲染循环
+    {
+        modelMatrix = glm::mat4( 1.0f );
+        viewMatrix = pCamera->getViewMatrix();
+
+        // 投影变换矩阵（透视投影）
+        projectionMatrix = glm::perspective(
+                        glm::radians( pCamera->fov() ),         // FOV，field of view，视角广度
+                        ( float ) ScreenWidth / ScreenHeight,   // aspectRatio，宽高比
+                        0.1f,                                   // near plane，近平面位置
+                        100.0f );                               // far plane，远平面位置
+        shader.use();
+        shader.setMat4( "modelMatrix", glm::mat4( 1.0f ) );
+        shader.setMat4( "viewMatrix", viewMatrix );
+        shader.setMat4( "projectionMatrix", projectionMatrix );
+
+        drawToCustomFrameBuffer();
+        drawToDefaultFrameBuffer();
+    } );
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    glDeleteVertexArrays( 1, &VAO_coord );
+    glDeleteVertexArrays( 1, &VAO_cube );
+    glDeleteVertexArrays( 1, &VAO_floor );
+
+    glDeleteBuffers( 1, &VBO_coord );
+    glDeleteBuffers( 1, &VBO_cube );
+    glDeleteBuffers( 1, &VBO_floor );
+
+    glDeleteProgram( shader.id );
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    glfwTerminate();
+    return 0;
+}
+
+void drawToCustomFrameBuffer()
+{
+    // ***************************************************
+    // ******* 使用自定义缓冲，后续的渲染数据会写入颜色附件（离屏渲染），深度值和模板值也有相应的附件保存。
+    // ***************************************************
+    glBindFramebuffer( GL_FRAMEBUFFER, frameBuffer );
+    glEnable( GL_DEPTH_TEST );
+
+    glClearColor( 0.1f, 0.1f, 0.1f, 0.1f );
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );       // 使用不使用模板缓冲
+
+    // ------------------------------------------
+    shader.setInt( "task", TASK_COORD );
+    glBindVertexArray( VAO_coord );
+    glDrawArrays( GL_LINES, 0, 6 );
+
+    // ------------------------------------------
+    shader.setInt( "task", TASK_OBJECT );
+    glBindVertexArray( VAO_cube );
+    glBindTexture( GL_TEXTURE_2D, textureCube );
+    shader.setMat4( "modelMatrix", glm::translate( glm::mat4( 1.0f ), glm::vec3( -1.0f, 0.0f, -1.0f ) ) );
+    glDrawArrays( GL_TRIANGLES, 0, 36 );
+    shader.setMat4( "modelMatrix", glm::translate( glm::mat4( 1.0f ), glm::vec3( 2.0f, 0.0f, 0.0f ) ) );
+    glDrawArrays( GL_TRIANGLES, 0, 36 );
+
+    // ------------------------------------------
+    shader.setInt( "task", TASK_OBJECT );
+    glBindVertexArray( VAO_floor );
+    glBindTexture( GL_TEXTURE_2D, textureFloor );
+    glDrawArrays( GL_TRIANGLES, 0, 6 );
+
+    glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+}
+
+void drawToDefaultFrameBuffer()
+{
+    // ***************************************************
+    // ******* 使用默认帧缓冲，恢复成正常默认，
+    // ***************************************************
+    glBindFramebuffer( GL_FRAMEBUFFER, 0 );
+    //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );          // 我们所看到的3D画面，其实就是一张平面图，即通过两个三角形组成的矩形绘制出来的。
+    glDisable( GL_DEPTH_TEST );                             // 就是平面图，不需要深度值了。
+
+    glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    shader.setInt( "task", TASK_SCREEN );
+    glBindTexture( GL_TEXTURE_2D, textureColorBuffer );     // 使用颜色附件数据来纹理贴图。
+
+    glBindVertexArray( VAO_screen );
+    glDrawArrays( GL_TRIANGLES, 0, 6 );
+}
+
+void renderLoop( GLFWwindow *window, function<void()> renderCallback )
+{
+    double currentFrame;
+    while( !glfwWindowShouldClose( window ) )
+    {
+        currentFrame = glfwGetTime();               // 计算帧间隔
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput( window );                     // 处理输入：键盘、鼠标事件等。
+
+        if( renderCallback != nullptr )             // render loop
+        {
+            renderCallback();
+        }
+
+        glfwSwapBuffers( window );                  // 双缓冲
+
+        // IO轮询，检查有没有触发什么事件（keys pressed/released, mouse moved etc.）,
+        // 并调用对应的回调函数（可以通过回调方法手动设置）,更新窗口状态，
+        glfwPollEvents();
+    }
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput( GLFWwindow *window )
+{
+    if( glfwGetKey( window, GLFW_KEY_ESCAPE ) == GLFW_PRESS )
+    {
+        glfwSetWindowShouldClose( window, true );
+    }
+    pCamera->processKeyboard( window, ( float ) deltaTime );
+}
