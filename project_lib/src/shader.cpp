085c76d8f519552f30812fe68bc48b4dabb3f780
Jackie <741212080@qq.com>
2021-02-04 17:37:23 +0800

    Shader类代码优化

--- project_lib/src/shader.cpp
+++ project_lib/src/shader.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void loadStringFromFile( const string &filePath, string &content );
+void loadShaderSrcFromFile( const string &filePath, string &content );
 
 Shader::Shader()
     : id( -1 )
@@ -11,22 +11,22 @@ Shader::Shader()
 
 }
 
-Shader::Shader( const string &vertexPath, const string &fragmentPath, const std::string &geometryPath )
+Shader::Shader( const string &vertexPath, const string &fragmentPath, const string &geometryPath )
 {
     initWithFile( vertexPath, fragmentPath, geometryPath );
 }
 
-void Shader::initWithFile( const string &vertexPath, const string &fragmentPath, const std::string &geometryPath )
+void Shader::initWithFile( const string &vertexPath, const string &fragmentPath, const string &geometryPath )
 {
     string vertexShaderSrc, fragmentShaderSrc, geometryShaderSrc;
-    loadStringFromFile( vertexPath, vertexShaderSrc );
-    loadStringFromFile( fragmentPath, fragmentShaderSrc );
-    loadStringFromFile( geometryPath, geometryShaderSrc );
+    loadShaderSrcFromFile( vertexPath, vertexShaderSrc );
+    loadShaderSrcFromFile( fragmentPath, fragmentShaderSrc );
+    loadShaderSrcFromFile( geometryPath, geometryShaderSrc );
 
     initWithSrc( vertexShaderSrc, fragmentShaderSrc, geometryShaderSrc );
 }
 
-void Shader::initWithSrc( const std::string &vertexShaderSrc, const std::string &fragmentShaderSrc, const std::string &geometryShaderSrc )
+void Shader::initWithSrc( const string &vertexShaderSrc, const string &fragmentShaderSrc, const string &geometryShaderSrc )
 {
     GLuint vertexShader = createAndCompileShader( GL_VERTEX_SHADER, vertexShaderSrc );
     GLuint fragmentShader = createAndCompileShader( GL_FRAGMENT_SHADER, fragmentShaderSrc );
@@ -51,7 +51,7 @@ void Shader::initWithSrc( const std::string &vertexShaderSrc, const std::string 
     if( !success )
     {
         glGetProgramInfoLog( id, lenLog, NULL, infoLog );
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << endl;
     }
 
     // 在把着色器对象链接到程序对象以后，记得删除着色器对象，我们不再需要它们了
@@ -68,62 +68,62 @@ void Shader::use() const
     glUseProgram( id );
 }
 
-void Shader::setBool( const std::string &name, GLboolean value ) const
+void Shader::setBool( const string &name, GLboolean value ) const
 {
     glUniform1i( glGetUniformLocation( id, name.c_str() ), ( int )value );
 }
 
-void Shader::setInt( const std::string &name, GLint value ) const
+void Shader::setInt( const string &name, GLint value ) const
 {
     glUniform1i( glGetUniformLocation( id, name.c_str() ), value );
 }
 
-void Shader::setFloat( const std::string &name, GLfloat value ) const
+void Shader::setFloat( const string &name, GLfloat value ) const
 {
     glUniform1f( glGetUniformLocation( id, name.c_str() ), value );
 }
 
-void Shader::setVec2( const std::string &name, const glm::vec2 &value ) const
+void Shader::setVec2( const string &name, const glm::vec2 &value ) const
 {
     glUniform2fv( glGetUniformLocation( id, name.c_str() ), 1, &value[0] );
 }
 
-void Shader::setVec2( const std::string &name, GLfloat x, GLfloat y ) const
+void Shader::setVec2( const string &name, GLfloat x, GLfloat y ) const
 {
     glUniform2f( glGetUniformLocation( id, name.c_str() ), x, y );
 }
 
-void Shader::setVec3( const std::string &name, const glm::vec3 &value ) const
+void Shader::setVec3( const string &name, const glm::vec3 &value ) const
 {
     glUniform3fv( glGetUniformLocation( id, name.c_str() ), 1, &value[0] );
 }
 
-void Shader::setVec3( const std::string &name, GLfloat x, GLfloat y, GLfloat z ) const
+void Shader::setVec3( const string &name, GLfloat x, GLfloat y, GLfloat z ) const
 {
     glUniform3f( glGetUniformLocation( id, name.c_str() ), x, y, z );
 }
 
-void Shader::setVec4( const std::string &name, const glm::vec4 &value ) const
+void Shader::setVec4( const string &name, const glm::vec4 &value ) const
 {
     glUniform4fv( glGetUniformLocation( id, name.c_str() ), 1, &value[0] );
 }
 
-void Shader::setVec4( const std::string &name, GLfloat x, GLfloat y, GLfloat z, GLfloat w )
+void Shader::setVec4( const string &name, GLfloat x, GLfloat y, GLfloat z, GLfloat w )
 {
     glUniform4f( glGetUniformLocation( id, name.c_str() ), x, y, z, w );
 }
 
-void Shader::setMat2( const std::string &name, const glm::mat2 &mat ) const
+void Shader::setMat2( const string &name, const glm::mat2 &mat ) const
 {
     glUniformMatrix2fv( glGetUniformLocation( id, name.c_str() ), 1, GL_FALSE, &mat[0][0] );
 }
 
-void Shader::setMat3( const std::string &name, const glm::mat3 &mat ) const
+void Shader::setMat3( const string &name, const glm::mat3 &mat ) const
 {
     glUniformMatrix3fv( glGetUniformLocation( id, name.c_str() ), 1, GL_FALSE, &mat[0][0] );
 }
 
-void Shader::setMat4( const std::string &name, const GLfloat *values ) const
+void Shader::setMat4( const string &name, const GLfloat *values ) const
 {
     GLint transformLoc = glGetUniformLocation( id, name.c_str() );
     glUniformMatrix4fv( transformLoc,   // 位置值
@@ -132,18 +132,18 @@ void Shader::setMat4( const std::string &name, const GLfloat *values ) const
                         values );       // 矩阵里元素数据
 }
 
-void Shader::setMat4( const std::string &name, const glm::mat4 &mat ) const
+void Shader::setMat4( const string &name, const glm::mat4 &mat ) const
 {
     glUniformMatrix4fv( glGetUniformLocation( id, name.c_str() ), 1, GL_FALSE, &mat[0][0] );
 }
 
-//GLuint Shader::createAndCompileShaderFromFile( GLenum shaderFlag, const std::string &shaderPath )
+//GLuint Shader::createAndCompileShaderFromFile( GLenum shaderFlag, const string &shaderPath )
 //{
 //    GLuint shader = -1;
-//    std::ifstream shaderFile( shaderPath );
+//    ifstream shaderFile( shaderPath );
 //    if( shaderFile.is_open() )
 //    {
-//        std::string shaderSrc( ( std::istreambuf_iterator<char>( shaderFile ) ), std::istreambuf_iterator<char>() );
+//        string shaderSrc( ( istreambuf_iterator<char>( shaderFile ) ), istreambuf_iterator<char>() );
 //        shader = createAndCompileShader( shaderFlag, shaderSrc );
 //    }
 //    shaderFile.close();
@@ -152,7 +152,7 @@ void Shader::setMat4( const std::string &name, const glm::mat4 &mat ) const
 
 
 // build and compile our shader program
-GLuint Shader::createAndCompileShader( GLenum shaderFlag, const std::string &shaderSrc )
+GLuint Shader::createAndCompileShader( GLenum shaderFlag, const string &shaderSrc )
 {
     GLuint shader = glCreateShader( shaderFlag );                   // 常见shader实例
     const char *tmpShaderSrc = shaderSrc.c_str();
@@ -167,27 +167,27 @@ GLuint Shader::createAndCompileShader( GLenum shaderFlag, const std::string &sha
     if( !success )
     {
         glGetShaderInfoLog( shader, lenLog, NULL, infoLog );        // 编译没有成功，获取编译日志
-        std::cout << ( shaderFlag == GL_VERTEX_SHADER ? "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" :
-                       shaderFlag == GL_FRAGMENT_SHADER ? "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" :
-                       shaderFlag == GL_GEOMETRY_SHADER ? "ERROR::SHADER::GEOMETRY::COMPILATION_FAILED\n" :
-                       "ERROR::SHADER::UNKNOWN::COMPILATION_FAILED\n" )
-                  << infoLog
-                  << std::endl;
+        cout << ( shaderFlag == GL_VERTEX_SHADER ? "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" :
+                  shaderFlag == GL_FRAGMENT_SHADER ? "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" :
+                  shaderFlag == GL_GEOMETRY_SHADER ? "ERROR::SHADER::GEOMETRY::COMPILATION_FAILED\n" :
+                  "ERROR::SHADER::UNKNOWN::COMPILATION_FAILED\n" )
+             << infoLog
+             << endl;
     }
     return shader;
 }
 
-void loadStringFromFile( const string &filePath, string &content )
+void loadShaderSrcFromFile( const string &filePath, string &content )
 {
     if( filePath == "" )
     {
         content = "";
         return;
     }
-    std::ifstream fileStream( filePath );
+    ifstream fileStream( filePath );
     if( fileStream.is_open() )
     {
-        content = string( ( std::istreambuf_iterator<char>( fileStream ) ), std::istreambuf_iterator<char>() );
+        content = string( ( istreambuf_iterator<char>( fileStream ) ), istreambuf_iterator<char>() );
         fileStream.close();
     }
     else
